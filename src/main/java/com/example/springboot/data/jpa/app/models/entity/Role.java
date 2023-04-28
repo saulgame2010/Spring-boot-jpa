330d8fef20d0e3d54126b49111dd298265eb97ad
saulgame2010 <saulgame2010@gmail.com>
2023-04-27 18:36:31 -0600

    Las clases para las entidades de usuario y rol fueron creadas
    Esto es para hacer autenticación con JPA

---
+++ src/main/java/com/example/springboot/data/jpa/app/models/entity/Role.java
@@ -0,0 +1,35 @@
+package com.example.springboot.data.jpa.app.models.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "authorities", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "authority"})})
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
